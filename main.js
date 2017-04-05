941c78d23af6fe2f062a03704b9edd5fbb9be565
Pauli Salazar <pisalazar@uc.cl>
2017-04-05 18:42:03 -0400

    inicio ejercicio

---
+++ main.js
@@ -0,0 +1,19 @@
+// Crear un repositorio
+// Al usuario se le va a pedir que ingrese 2 numeros. 
+// Se le debe mostrar al usuario en una alerta los 4 posibles resultados de las operaciones matematicas
+// con estos dos numeros
+// Suben el codigo a github
+// Comparten link en Slack
+
+
+var numero1=Number(prompt("Ingresa un numero"))
+var numero2=Number(prompt("Ingresa otro numero"))
+var suma= numero1+numero2;
+var resta= numero1-numero2;
+var multiplicacion= numero1*numero2;
+var division= numero1/numero2;
+
+alert("La suma es: " + suma)
+alert("La resta es: " + resta)
+alert("La multiplicación es: " + multiplicacion)
+alert("La division es: " + division)
